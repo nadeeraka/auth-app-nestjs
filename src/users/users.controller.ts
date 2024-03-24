123a2544ceae53fdc2cb4dfbab500402c1362e6a
nadeeraka <global-nadeerakan@gmail.com>
2024-03-24 22:30:05 +0530

    pipes add

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ZodValidationPipe } from 'src/utils/common/ZodValidationPipe';
+import { createCatSchema } from './entities/zod/createUserSchema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(createCatSchema))
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
