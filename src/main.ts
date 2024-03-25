ea7498c4385abee7728a4823063a03b1d793f6f0
nadeeraka <global-nadeerakan@gmail.com>
2024-03-25 14:08:58 +0530

    AllExceptionsFilter  add

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
+  // add exception layer for more readable errors
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   app.setGlobalPrefix('api');
 
   const options = new DocumentBuilder()
     .setTitle('Api documentation')
     .setVersion('1.0')
-    // .setBasePath('api')
     .addBearerAuth()
-
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
-//   set as public api
-app.enableCors()
+
   await app.listen(8000);
   console.log(`Application is running on 🚀: ${await app.getUrl()}`);
 }
